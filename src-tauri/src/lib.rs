cc51419e5abafb5dbe3a21eff563ed37c995a00a
VisualSource <collin_blosser@yahoo.com>
2024-09-16 16:21:48 -0400

    fix updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,11 @@
-use tauri_plugin_dialog::DialogExt;
-use tauri_plugin_updater::UpdaterExt;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move { update(&handle).await });
+            #[cfg(desktop)]
+            let _ = app
+                .handle()
+                .plugin(tauri_plugin_updater::Builder::new().build());
             Ok(())
         })
         .plugin(tauri_plugin_fs::init())
@@ -15,28 +14,4 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-async fn update(app: &tauri::AppHandle) -> tauri::Result<()> {
-    if let Some(update) = app
-        .updater()
-        .map_err(|e| tauri::Error::Anyhow(e.into()))?
-        .check()
-        .await
-        .map_err(|e| tauri::Error::Anyhow(e.into()))?
-    {
-        update
-            .download_and_install(|_, _| {}, || {})
-            .await
-            .map_err(|e| tauri::Error::Anyhow(e.into()))?;
-        let result = app
-            .dialog()
-            .message("New update ready: Would you like to restart?")
-            .blocking_show();
-        if result {
-            app.restart();
-        }
-    }
-
-    Ok(())
 }
